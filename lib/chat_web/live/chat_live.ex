2f2c300813ba36a86471fa7533992f93f5bfdf82
ShenPrime <splinteredsanity@outlook.com>
2024-04-03 19:49:13 +0200

    Added: toast when user joins

--- lib/chat_web/live/chat_live.ex
+++ lib/chat_web/live/chat_live.ex
@@ -6,13 +6,18 @@ defmodule ChatWeb.ChatLive do
   def mount(_params, _session, socket) do
     if connected?(socket) do
       PubSub.subscribe(Chat.PubSub, "messages")
+      PubSub.subscribe(Chat.PubSub, "new_user")
     end
+
+    handle = MnemonicSlugs.generate_slug(2)
+
+    PubSub.broadcast_from!(Chat.PubSub, self(), "new_user", {:new_user, handle})
 
     socket =
       socket
       |> assign(:message, "")
       |> assign(:messages, [])
-      |> assign(:handle, MnemonicSlugs.generate_slug(2))
+      |> assign(:handle, handle)
 
     {:ok, socket}
   end
@@ -39,6 +44,21 @@ defmodule ChatWeb.ChatLive do
       socket
       |> assign(:messages, socket.assigns.messages ++ [{message, handle}])
 
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:new_user, handle}, socket) do
+    socket = put_flash(socket, :info, "User #{handle} has joined the chat")
+    send(self(), :clear_flash)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:clear_flash, socket) do
+    Process.sleep(2000)
+    socket = clear_flash(socket)
     {:noreply, socket}
   end
 end
